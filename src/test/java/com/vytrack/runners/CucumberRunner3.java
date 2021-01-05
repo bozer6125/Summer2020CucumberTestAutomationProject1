a65d8997564f17af1129f87ac062563573416ea1
bozer6125 <ozrbeyza@gmail.com>
2021-01-05 00:20:42 -0700

    files added

---
+++ src/test/java/com/vytrack/runners/CucumberRunner3.java
@@ -0,0 +1,31 @@
+package com.vytrack.runners;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber3.json",
+                "timeline:target/timeline-report"
+        },
+        features = {
+                "src/test/resources/features/Login.feature"
+        },
+        glue = "com/vytrack/step_definitions",
+        dryRun = true,
+        tags = "",
+        publish = true
+)
+public class CucumberRunner3 {
+    //tags = "@s_o and @with_two_columns",
+    // scenario must have BOTH tags
+    //java logic if(@s_o && @with_two_columns)
+
+    //tags = "@s_o or @with_two_columns",
+    // scenario must have EITHER OR tags
+    //java logic if(@s_o || @with_two_columns),
+}
---
+++ target/timeline-report/report.js
@@ -0,0 +1,142 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "eac3991b-dd8b-4503-9b09-1f1ea75f4d9e",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Login as a sales manger",
+    "start": 1609043388716,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@smoke,",
+    "end": 1609043388883,
+    "className": "passed"
+  },
+  {
+    "id": "c1fb93fd-4461-457a-9c37-c87016c7dcd7",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as sales manager",
+    "start": 1609043388918,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,",
+    "end": 1609043388932,
+    "className": "passed"
+  },
+  {
+    "id": "9763cf65-3924-42ba-9160-54d59adf620f",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as driver",
+    "start": 1609043389150,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,@with_two_columns,",
+    "end": 1609043389153,
+    "className": "passed"
+  },
+  {
+    "id": "8bb44ac0-36d9-4881-80ff-34240e83d4f2",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as store manager",
+    "start": 1609043388968,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,",
+    "end": 1609043388977,
+    "className": "passed"
+  },
+  {
+    "id": "9f8fd662-24a4-45a1-a375-31adaec874a2",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as driver",
+    "start": 1609043389029,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,",
+    "end": 1609043389047,
+    "className": "passed"
+  },
+  {
+    "id": "098e1204-dcca-4b5c-8020-9bde3b2cb5bd",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Invalid login with wrong12 and bad",
+    "start": 1609043389329,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@negative_scenario_outline,@b20-182,",
+    "end": 1609043389332,
+    "className": "passed"
+  },
+  {
+    "id": "e26a3fa2-5fbd-47f9-94b8-1c65d9e904ef",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as sales manager",
+    "start": 1609043389079,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,@with_two_columns,",
+    "end": 1609043389086,
+    "className": "passed"
+  },
+  {
+    "id": "5dbc859e-9f3d-49ea-946d-167208d2f8f0",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Invalid login with wrong213 and bad",
+    "start": 1609043389271,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@negative_scenario_outline,@b20-182,",
+    "end": 1609043389276,
+    "className": "passed"
+  },
+  {
+    "id": "05942ed8-7fba-40bc-9a3c-4266439471ad",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Invalid login with wrong32 and bad",
+    "start": 1609043389296,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@negative_scenario_outline,@b20-182,",
+    "end": 1609043389302,
+    "className": "passed"
+  },
+  {
+    "id": "60afbd8c-e3bd-430d-8c7e-48a4ed383a12",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Parametrized login as store manager",
+    "start": 1609043389126,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@s_o,@with_two_columns,",
+    "end": 1609043389133,
+    "className": "passed"
+  },
+  {
+    "id": "b241e89e-4f76-4e43-b14f-fc1a450996f2",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Invalid password",
+    "start": 1609043389179,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@negative_login,@smoke,",
+    "end": 1609043389212,
+    "className": "passed"
+  },
+  {
+    "id": "fc740a1f-d441-4f02-ba9d-4d8e27bf27d1",
+    "feature": "As user I want to be able to login under different roles",
+    "scenario": "Invalid login with wrong and bad",
+    "start": 1609043389230,
+    "group": 1,
+    "content": "",
+    "tags": "@login,@b20-183,@negative_scenario_outline,@b20-182,",
+    "end": 1609043389235,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
